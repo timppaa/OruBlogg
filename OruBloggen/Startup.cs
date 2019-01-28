61fbf9af4d7a26a15d821472096a780b6dbe2d65
Tim Olsson <tim-olsson@hotmail.com>
2019-01-28 15:01:00 +0100

    Add project files.

---
+++ OruBloggen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OruBloggen.Startup))]
+namespace OruBloggen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
