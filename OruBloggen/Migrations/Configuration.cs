fd5e14150b17141e24c3e1d6d6458f7fab3086d6
Martin <martinfalck21@gmail.com>
2019-01-30 13:16:40 +0100

    Skapade databasen

---
+++ OruBloggen/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace OruBloggen.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OruBloggen.Models.OruBloggenDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(OruBloggen.Models.OruBloggenDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ OruBloggen/Models/ReportModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OruBloggen.Models
+{
+    public class ReportModel
+    {
+        [Key]
+        public int ReportID { get; set; }
+        public string ReportReason { get; set; }
+
+        [InverseProperty("ReportModel")]
+        public virtual List<PostReportModel> PostReportModels { get; set; }
+
+    }
+}
