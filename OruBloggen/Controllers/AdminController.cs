c4d167a2af78a37e426b0ecee2b4cf095b13cfc0
Ludvig seiko <ludvigseiko@hotmail.com>
2019-02-01 11:12:51 +0100

    Lite admingrejer.

---
+++ OruBloggen/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using OruBloggen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OruBloggen.Controllers
+{
+    public class AdminController : Controller
+    {
+        public OruBloggenDbContext ctx = new OruBloggenDbContext();
+        public AdminViewModel AVM = new AdminViewModel();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Posts ()
+        {
+
+            ctx.Posts.Add(new PostModel() {
+                PostTitle = "Hej",
+                PostDate = DateTime.Now,
+                PostFormal = true,
+                PostText = "Första Inlägget",
+                PostUserID = "5a55a092-765d-4948-bf31-e43062ba3c40",
+            });
+            ctx.SaveChanges();
+            
+
+            AVM.postModels = ctx.Posts.ToList();
+
+            return View(AVM);
+        }
+    }
+}
---
+++ OruBloggen/Models/AdminViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OruBloggen.Models
+{
+    public class AdminViewModel
+    {
+        public List<PostModel> postModels { get; set; }
+
+        public ReportModel reportModel { get; set; }
+
+        public PostReportModel postReportModel { get; set; }
+
+        public UserModel userModel { get; set; }
+    }
+}
