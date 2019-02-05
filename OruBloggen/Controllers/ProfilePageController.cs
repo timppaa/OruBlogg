92bf119842a94fe2a5d5841ef99ee1fd8f97a76d
Tim Olsson <tim-olsson@hotmail.com>
2019-02-05 16:00:41 +0100

    Fixade en profilsida, la till befattning på registrering, ändra lite design på bilder

---
+++ OruBloggen/Controllers/ProfilePageController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using OruBloggen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OruBloggen.Controllers
+{
+    public class ProfilePageController : Controller
+    {
+        // GET: ProfilePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowInfo()
+        {
+            var ctx = new OruBloggenDbContext();
+            var identityCtx = new ApplicationDbContext();
+
+
+            var userId = User.Identity.GetUserId();
+            var Users = ctx.Users.Find(userId);
+            var identityUser = identityCtx.Users.Find(userId);
+            var teamId = Users.UserTeamID;
+            var team = ctx.Teams.FirstOrDefault(t => t.TeamID == teamId).TeamName;
+            var path = "/Images/" + Users.UserImagePath;
+
+            var model = new ProfilePageViewModel
+            {
+            /*model.*/ImagePath = path,
+            /*model.*/Firstname = Users.UserFirstname,
+            /*model.*/Lastname = Users.UserLastname,
+            /*model.*/Email = identityUser.Email,
+            /*model*/PhoneNumber = Users.UserPhoneNumber,
+            /*model.*/Team = team,
+            /*model.*/Position = Users.UserPosition
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ OruBloggen/Models/ProfilePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OruBloggen.Models
+{
+    public class ProfilePageViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Team { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Position { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
