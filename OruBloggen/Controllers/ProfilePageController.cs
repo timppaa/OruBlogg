f3beb0f87cd44baec94136ad5768d425965f2915
Kalle <karl.ekwall@hotmail.com>
2019-02-14 10:59:48 +0100

    Småfixat med profilsidan/meddelanden

--- OruBloggen/Controllers/ProfilePageController.cs
+++ OruBloggen/Controllers/ProfilePageController.cs
@@ -21,41 +21,48 @@ namespace OruBloggen.Controllers
             return RedirectToAction("ShowOtherUser", new { id = User.Identity.GetUserId() });
         }
 
-        //public ActionResult ShowInfo()
-        //{
-        //    var ctx = new OruBloggenDbContext();
-        //    var identityCtx = new ApplicationDbContext();
+        public ActionResult ShowInfo()
+        {
+            var ctx = new OruBloggenDbContext();
+            var identityCtx = new ApplicationDbContext();
 
 
-        //    var userId = User.Identity.GetUserId();
-        //    var Users = ctx.Users.Find(userId);
-        //    var identityUser = identityCtx.Users.Find(userId);
-        //    var teamId = Users.UserTeamID;
-        //    var team = ctx.Teams.FirstOrDefault(t => t.TeamID == teamId).TeamName;
-        //    var path = "/Images/" + Users.UserImagePath;
+            var userId = User.Identity.GetUserId();
+            var Users = ctx.Users.Find(userId);
+            var identityUser = identityCtx.Users.Find(userId);
+            var teamId = Users.UserTeamID;
+            var team = ctx.Teams.FirstOrDefault(t => t.TeamID == teamId).TeamName;
+            var path = "/Images/" + Users.UserImagePath;
 
             var MeetingModels = ctx.Meetings.ToList();
             var UserMeetings = ctx.UserMeetings.Where(u => u.UserID.Equals(userId)).ToList();
 
             var model = new ProfilePageViewModel
             {
-                      UserID = userId,
-            /*model.*/ImagePath = path,
-            /*model.*/Firstname = Users.UserFirstname,
-            /*model.*/Lastname = Users.UserLastname,
-            /*model.*/Email = identityUser.Email,
-            /*model*/ PhoneNumber = Users.UserPhoneNumber,
-            /*model.*/Team = team,
-            /*model.*/Position = Users.UserPosition,
-                      MeetingModels = MeetingModels,
-                      UserMeetings = UserMeetings,
-                      UserEmailNotification = Users.UserEmailNotification,
-                      UserPmNotification = Users.UserPmNotification,
-                      UserSmsNotification = Users.UserSmsNotification
+                UserID = userId,
+                /*model.*/
+                ImagePath = path,
+                /*model.*/
+                Firstname = Users.UserFirstname,
+                /*model.*/
+                Lastname = Users.UserLastname,
+                /*model.*/
+                Email = identityUser.Email,
+                /*model*/
+                PhoneNumber = Users.UserPhoneNumber,
+                /*model.*/
+                Team = team,
+                /*model.*/
+                Position = Users.UserPosition,
+                MeetingModels = MeetingModels,
+                UserMeetings = UserMeetings,
+                UserEmailNotification = Users.UserEmailNotification,
+                UserPmNotification = Users.UserPmNotification,
+                UserSmsNotification = Users.UserSmsNotification
             };
 
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
         public ActionResult ShowOtherUser(string id)
         {
